a46c5c018840d6716f2de8afb2ec5118b52d93dc
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-02-09 11:28:53 -0700

    Tons of changes!

--- SkiTime/wwwroot/js/site.js
+++ SkiTime/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    $(document).ready(function() {
+        $('#positiveIntegerInput').on('input', function () {
+            var value = $(this).val();
+            // Check if the value is not a positive integer
+            if (!/^\d+$/.test(value) && value != '') {
+                // If not, remove the last character entered
+                $(this).val(value.replace(/[^0-9]/g, ''));
+            }
+        });
+});
+
+$(document).ready(function() {
+    $('#calculateButton').click(function () {
+
+        var inputValue = parseInt($('#positiveIntegerInput').val(), 10);
+
+        if (!isNaN(inputValue)) {
+
+            var calculatedValue = inputValue * 55;
+
+            $('#resultBox').text(calculatedValue);
+
+        }
+        else {
+            $('#resultBox').text('Please enter a valid integer.');
+        }
+
+              
+    
+    // Display the calculated float in the resultBox
+    //$('#resultBox').text(calculatedFloat.toFixed(2)); // .toFixed(2) limits the float to 2 decimal places
+  });
+});
+
+
